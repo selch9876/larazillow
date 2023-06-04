e238b079a4d086f068a1e42c61c8db6f53356734
Selçuk Oktay <selcukoktay@gmail.com>
2023-06-04 22:32:14 +0300

    tailwind added

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,15 +1,27 @@
 <template>
-    <Link href="/">Index Page</Link>&nbsp;
-    <Link href="/hello">Show Page</Link>&nbsp;
-    <Link href="/listing">Listings</Link>&nbsp;
-    <Link href="/listing/create">New Listing</Link>&nbsp;
-    <!-- <div>The page timer {{ timer}}</div> -->
-
-    <div v-if="flashSuccess" class="success">
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+      <div class="container mx-auto">
+        <nav class="p-4 flex items-center justify-between">
+          <div class="text-lg font-medium">
+            <Link :href="route('listing.index')">Listings</Link>
+          </div>
+          <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+            <Link :href="route('listing.index')">LaraZillow</Link>
+          </div>
+          <div>
+            <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  
+    <main class="container mx-auto p-4">
+      <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
         {{ flashSuccess }}
-    </div>
-    <slot></slot>
-</template>
+      </div>
+      <slot>Default</slot>
+    </main>
+  </template>
 
 <script setup>
 import {Link, usePage} from '@inertiajs/vue3'
@@ -18,13 +30,4 @@ import { computed } from 'vue';
 
 const flashSuccess = computed(() => usePage().props.flash.success)
 
-// const timer = ref(0)
-// setInterval(() => timer.value++, 1000)
 </script>
-
-<style scoped>
-    .success {
-        background-color: green;
-        color: white;
-    }
-</style>
