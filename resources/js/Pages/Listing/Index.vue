a9745d380fe3c72fd2cfbf9743db671cc3bf17c0
selch9876 <32863267+selch9876@users.noreply.github.com>
2023-06-09 11:21:33 +0300

    composable added

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,7 +3,12 @@
         <Box v-for="listing in listings" :key="listing.id">
             <div>
                 <Link :href="route('listing.show', {listing: listing.id})">
-                    <Price :price="listing.price" class="text-2xl font-bold"/>
+                    <div class="flex items-center gap-1">
+                        <Price :price="listing.price" class="text-2xl font-bold"/>
+                        <div class="text-xs text-gray-500">
+                            <Price :price="400"  />pm
+                        </div>
+                    </div>
                     <ListingSpace :listing="listing" class="text-lg" />
                     <ListingAddress :listing="listing" class="text-gray-500"/>
                 </Link>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -17,17 +17,17 @@
                     Monthly Payment
                 </template>
                 <div>
-                    <label class="label">Interest Rate (2.5%)</label>
-                    <input type="range" min="0.1" max="30" step="0.1" 
+                    <label class="label">Interest Rate: ({{interestRate}}%)</label>
+                    <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1" 
                     class="w-full bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
 
-                    <label class="label">Duration (25 Years)</label>
-                    <input type="range" min="3" max="35" step="1" 
+                    <label class="label">Duration: ({{ duration }} years)</label>
+                    <input v-model.number="duration" type="range" min="3" max="35" step="1" 
                     class="w-full bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
 
                     <div class="text-gray-600 dark:text-gray-300 mt-2">
                         <div class="text-gray-300"> Your Monthly Payment</div>
-                        <Price :price="500" class="text-3xl" />
+                        <Price :price="monthlyPayment" class="text-3xl" />
                     </div>
                 </div>
             </Box>
@@ -41,7 +41,17 @@ import ListingAddress from '@/Components/ListingAddress.vue';
 import Price from '@/Components/Price.vue';
 import ListingSpace from '@/Components/ListingSpace.vue';
 import Box from '@/Components/UI/Box.vue';
-defineProps({
+import {ref} from 'vue';
+import {useMonthlyPayment} from '@/Composables/useMonthlyPayment'
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
     listing: Object,
 })
+
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration)
+
+
 </script>
