b47947634bf64496d3eaf1f881d2c5f26325172c
selch9876 <32863267+selch9876@users.noreply.github.com>
2023-06-20 15:58:08 +0300

    realtor filters started

---
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+        <div class="mb-4 flex flex-wrap gap-2">
+            <div class="flex flex-nowrap items-center gap-2">
+                <input v-model="filterForm.deleted" type="checkbox" id="deleted" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
+                <label for="deleted">Deleted</label>
+                <input type="checkbox" id="drafts" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
+                <label for="drafts">Drafts</label>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import { reactive, watch } from 'vue';
+
+const filterForm = reactive({
+    deleted: false,
+})
+
+// reactive , ref or computed
+watch(filterForm, () => console.log('change detected'))
+</script>
