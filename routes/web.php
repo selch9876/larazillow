be7f6b404dbc584fc5e591358d82f4e18d42deb0
selcuk oktay <selcukoktay@gmail.com>
2024-09-18 00:10:26 +0300

    Offers keeps on

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\ListingOfferController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
 use App\Http\Controllers\UserAccountController;
@@ -28,7 +29,9 @@ Route::get('/hello', [IndexController::class, 'show']);
 Route::resource('listing', ListingController::class)
   ->only(['index', 'show']);
   
-
+  Route::resource('listing.offer', ListingOfferController::class)
+  ->middleware('auth')
+  ->only(['store']);
 // Auth Routes
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
